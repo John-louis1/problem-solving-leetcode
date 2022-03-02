f13b7548d9d0c5245bb6a501b40068e03f316f53
John Fayez Louis <97978852+John-louis1@users.noreply.github.com>
2022-03-02 20:23:20 +0200

    Time: 0 ms (100.00%), Space: 41 MB (20.05%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,50 +1,23 @@
-// Bottom-top approach
 class Solution {
-    static int memo[];
-
-    static void generateMemoArray() {
-        memo = new int [38];
-        memo[0] = 0; 
-        memo[1] = 1; 
-        memo[2] = 1; 
-        for (int i = 3; i <= 37; i++) {
-            memo[i] = memo[i-3] + memo[i-2] + memo[i- 1];
-        }
-    }
-
-    static int tribonacci(int n) {
-        generateMemoArray();
-        return memo[n];
-    }
-}
-
-/*
-
-Top-bottom approach
-
-class Solution {
-    
-    static int memo[];
-    
     public int tribonacci(int n) {
-        memo = new int[38];
-        return solve(n);
-    }
-    
-    public static int solve(int n) {
-        if (n == 0) 
-            return 0;
-        if (n == 1) 
+        if(n<2){
+            return n;
+        }
+        if(n==2){
             return 1;
-        if (n == 2) 
-            return 1;
-        if (memo[n] != 0)
-            return memo[n];
-        int res = solve(n-3) + solve(n-2) + solve(n-1);
-        memo[n] = res;
-        return res;
+        }
+        if(n==3){
+            return 2;
+        }
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1]=1;
+        dp[2]=1;
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-3]+dp[i-2]+dp[i-1];
+        }
+        
+        return dp[n];
+        
     }
-    
 }
-
-*/

e786039dde88b1d8d521cf7b4395b1749b4f2bcc
John Fayez Louis <97978852+John-louis1@users.noreply.github.com>
2022-03-02 20:05:41 +0200

    Time: 0 ms (100.00%), Space: 40.9 MB (24.14%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -2,18 +2,18 @@
 class Solution {
     static int memo[];
     
-    static void generate() {
+    static void generate(int n) {
         memo = new int [38];
         memo[0] = 0; 
         memo[1] = 1; 
         memo[2] = 1; 
-        for (int i = 3; i <= 37; i++) {
+        for (int i = 3; i <= n; i++) {
             memo[i] = memo[i-3] + memo[i-2] + memo[i- 1];
         }
     }
     
     static int tribonacci(int n) {
-        generate();
+        generate(n);
         return memo[n];
     }
 }

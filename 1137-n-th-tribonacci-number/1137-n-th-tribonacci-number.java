a78b45fc456d86bff1b2c6cf526425f17d89ee3e
John Fayez Louis <97978852+John-louis1@users.noreply.github.com>
2022-03-02 20:24:13 +0200

    Time: 0 ms (100.00%), Space: 40.6 MB (33.81%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,3 +1,4 @@
+// Bottom-top approach
 class Solution {
     static int memo[];
 
@@ -16,3 +17,34 @@ class Solution {
         return memo[n];
     }
 }
+
+/*
+
+Top-bottom approach
+
+class Solution {
+    
+    static int memo[];
+    
+    public int tribonacci(int n) {
+        memo = new int[38];
+        return solve(n);
+    }
+    
+    public static int solve(int n) {
+        if (n == 0) 
+            return 0;
+        if (n == 1) 
+            return 1;
+        if (n == 2) 
+            return 1;
+        if (memo[n] != 0)
+            return memo[n];
+        int res = solve(n-3) + solve(n-2) + solve(n-1);
+        memo[n] = res;
+        return res;
+    }
+    
+}
+
+*/
